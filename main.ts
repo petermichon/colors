4f1a8043accaaeb67c74e1badba1a7cbedd7f7a8
shrln <shrln0@proton.me>
2024-10-07 16:05:57 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,18 @@
-// import { Canvas } from "./io/canvas";
-// import { World } from "./world";
+import { Canvas } from "./io/canvas";
+import { World } from "./world";
 
-// import { loadEntities } from "./colors";
-
-// export function main() {
-//     loadEntities();
-
-//     {
-//         const e = World.query((e) => e.setMouse)[0];
-//         const x = Math.trunc(Canvas.getWidth() / 2);
-//         const y = Math.trunc(Canvas.getHeight() / 2);
-//         e.setMouse(x, y);
-//         e.setMouse = undefined;
-//     }
-
-//     World.query((e) => e.beginAnimation)[0].beginAnimation();
-// }
+import { loadEntities } from "./colors";
 
 export function main() {
-    console.log("main");
+    loadEntities();
+
+    {
+        const e = World.query((e) => e.setMouse)[0];
+        const x = Math.trunc(Canvas.getWidth() / 2);
+        const y = Math.trunc(Canvas.getHeight() / 2);
+        e.setMouse(x, y);
+        e.setMouse = undefined;
+    }
+
+    World.query((e) => e.beginAnimation)[0].beginAnimation();
 }
