ee2f707a5492893b0e18284ba1686ed3b11781d2
shrln <shrln0@proton.me>
2024-10-07 21:22:51 +0200

    Three.js fix

--- main.ts
+++ main.ts
@@ -3,9 +3,9 @@
 
 // import { loadEntities } from "./colors";
 
+import * as THREE from "three";
+
 export function main() {
-    console.log("test");
-    console.log("test");
     // loadEntities();
     // {
     //     const e = World.query((e) => e.setMouse)[0];
@@ -16,4 +16,35 @@ export function main() {
     // }
     // console.log("test1");
     // World.query((e) => e.beginAnimation)[0].beginAnimation();
+    animate(1000);
+}
+
+const width = window.innerWidth,
+    height = window.innerHeight;
+
+// init
+
+const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
+camera.position.z = 1;
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+const material = new THREE.MeshNormalMaterial();
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(width, height);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+// animation
+
+function animate(time: number) {
+    mesh.rotation.x = time / 2000;
+    mesh.rotation.y = time / 1000;
+
+    renderer.render(scene, camera);
 }
