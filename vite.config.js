bc882203e36c4ba4a96b1955e95b3dd72082ac1d
shrln <shrln0@proton.me>
2024-10-23 19:30:49 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,11 @@
+import { defineConfig } from "vite";
+
 export default {
     // config options
+    // root: "./public",
+    root: ".",
 };
+
+// export default defineConfig({
+//     root: "public", // Change this to the folder where your index.html is located
+// });
