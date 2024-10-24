f4ad918a82f3fd27bd6951f58fa0348bdce1d1b9
shrln <shrln0@proton.me>
2024-10-24 17:43:30 +0200

    Some graphics experimenting

--- src/app/main.ts
+++ src/app/main.ts
@@ -5,26 +5,28 @@
 
 import * as THREE from "three";
 
-export function old() {
-    // loadEntities();
-    // {
-    //     const e = World.query((e) => e.setMouse)[0];
-    //     const x = Math.trunc(Canvas.getWidth() / 2);
-    //     const y = Math.trunc(Canvas.getHeight() / 2);
-    //     e.setMouse(x, y);
-    //     e.setMouse = undefined;
-    // }
-    // console.log("test1");
-    // World.query((e) => e.beginAnimation)[0].beginAnimation();
-}
+// function old() {
+// loadEntities();
+// {
+//     const e = World.query((e) => e.setMouse)[0];
+//     const x = Math.trunc(Canvas.getWidth() / 2);
+//     const y = Math.trunc(Canvas.getHeight() / 2);
+//     e.setMouse(x, y);
+//     e.setMouse = undefined;
+// }
+// console.log("test1");
+// World.query((e) => e.beginAnimation)[0].beginAnimation();
+// }
+
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 export default function main() {
     const width = window.innerWidth;
     const height = window.innerHeight;
 
     // Camera
-    const camera = new THREE.PerspectiveCamera(90, width / height);
-    camera.position.z = 1;
+    const camera = new THREE.PerspectiveCamera(75, width / height);
+    camera.position.set(8, 4, 8);
 
     // Scene
     const scene = new THREE.Scene();
@@ -35,12 +37,38 @@ export default function main() {
     renderer.setAnimationLoop(animate);
     document.body.appendChild(renderer.domElement);
 
-    // Elements
-    const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-    const material = new THREE.MeshNormalMaterial();
+    // Controls
+    const controls = new OrbitControls(camera, renderer.domElement);
 
-    const mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+    // Boxes
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshStandardMaterial();
+
+    {
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.position.set(-1.5, 0.5, -1.5);
+        scene.add(mesh);
+    }
+    {
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.position.set(1.5, 0.5, 1.5);
+        scene.add(mesh);
+    }
+
+    // Lights
+    const light = new THREE.PointLight(0xffffff, 1);
+    light.position.set(-3.5, 2, -9.5);
+    scene.add(light);
+
+    // Helpers
+    const gridSize = 50;
+    const gridHelper = new THREE.GridHelper(gridSize, gridSize);
+    scene.add(gridHelper);
+
+    const lightHelper = new THREE.PointLightHelper(light);
+    scene.add(lightHelper);
+
+    controls.update();
 
     function animate(time: number) {
         {
@@ -50,8 +78,10 @@ export default function main() {
             renderer.setSize(width, height);
         }
 
-        mesh.rotation.x = time / 2000;
-        mesh.rotation.y = time / 1000;
+        // mesh.rotation.x = time / 2000;
+        // mesh.rotation.y = time / 1000;
+        light.position.x += Math.cos(time / 1000) * 0.1;
+        light.position.z += Math.sin(time / 1000) * 0.1;
 
         renderer.render(scene, camera);
     }
