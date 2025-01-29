aeda61840ff9a6f8a915405b7c6028ac544c1a5e
petermichon <peter.michon@proton.me>
2025-01-29 11:35:52 +0100

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -6,24 +6,29 @@ self.addEventListener('activate', () => {
   console.log('activate') // DEBUG
 })
 
+// client -> worker
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      // If a cached response is found, return it
-      const cacheResponseFound = cachedResponse !== undefined
-      if (cacheResponseFound) {
-        return cachedResponse
-      }
+  const response = caches.match(event.request).then((cachedResponse) => {
+    // If a cached response is found, return it
+    const cachedResponseFound = cachedResponse !== undefined
+    if (cachedResponseFound) {
+      return cachedResponse
+    }
 
-      // If no cached response
-      // Fetch the request from the network
-      return fetch(event.request).then((networkResponse) => {
-        // Cache the network response for future use
-        return caches.open('v1').then((cache) => {
-          cache.put(event.request, networkResponse.clone())
-          return networkResponse
-        })
-      })
-    })
-  )
+    // If no cached response
+
+    // worker -> host
+    const networkResponse = fetch(event.request).then(
+      async (networkResponse) => {
+        // worker <- host
+        const cache = await caches.open('v1')
+        cache.put(event.request, networkResponse.clone())
+        return networkResponse
+      }
+    )
+    return networkResponse
+  })
+
+  // client <- worker
+  event.respondWith(response)
 })
