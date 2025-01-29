76b55940ed184160ea293b7e4c748a5c1f6ed1a5
petermichon <peter.michon@proton.me>
2025-01-29 15:40:41 +0100

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -9,30 +9,27 @@ self.addEventListener('activate', () => {
 })
 
 self.addEventListener('fetch', (event) => {
-  const response = (async () => {
+  async function f() {
     try {
-      // Fetch from network
       const networkResponse = await fetch(event.request)
 
-      // If successful, cache the network response
       const cache = await caches.open('v1')
       cache.put(event.request, networkResponse.clone())
 
       return networkResponse
-    } catch (error) {
-      // If network fails, check the cache
+    } catch {
       const cache = await caches.open('v1')
       const cachedResponse = await cache.match(event.request)
 
-      //If successful, return cached response
       if (cachedResponse !== undefined) {
         return cachedResponse
+      } else {
+        return new Response('Offline', { status: 503 })
       }
-
-      // If no cache available, return offline response
-      return new Response('Offline', { status: 503 })
     }
-  })()
+  }
+
+  const response = f()
 
   event.respondWith(response)
 })
