46bc08003883db95ca67c5b096503e22c34a3d80
petermichon <peter.michon@proton.me>
2025-01-29 12:10:24 +0100

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,38 +1,38 @@
 self.addEventListener('install', () => {
   console.log('install') // DEBUG
-  self.skipWaiting()
+  // self.skipWaiting()
 })
 
 self.addEventListener('activate', () => {
   console.log('activate') // DEBUG
-  self.clients.claim()
+  // self.clients.claim()
 })
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    (async () => {
-      try {
-        // Try fetching from the network
-        const networkResponse = await fetch(event.request)
+  const response = (async () => {
+    try {
+      // Fetch from network
+      const networkResponse = await fetch(event.request)
 
-        // Cache the network response if successful
-        const cache = await caches.open('dynamic-cache')
-        cache.put(event.request, networkResponse.clone())
+      // If successful, cache the network response
+      const cache = await caches.open('v1')
+      cache.put(event.request, networkResponse.clone())
 
-        return networkResponse
-      } catch (error) {
-        // If network fails, check the cache
-        const cache = await caches.open('dynamic-cache')
-        const cachedResponse = await cache.match(event.request)
+      return networkResponse
+    } catch (error) {
+      // If network fails, check the cache
+      const cache = await caches.open('v1')
+      const cachedResponse = await cache.match(event.request)
 
-        // Return cached response or fallback to a default (offline page, etc.)
-        if (cachedResponse) {
-          return cachedResponse
-        }
-
-        // If no cache available, return a default offline response (optional)
-        return new Response('Offline', { status: 503 })
+      //If successful, return cached response
+      if (cachedResponse !== undefined) {
+        return cachedResponse
       }
-    })()
-  )
+
+      // If no cache available, return offline response
+      return new Response('Offline', { status: 503 })
+    }
+  })()
+
+  event.respondWith(response)
 })
