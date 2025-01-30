68a66973ec51c0a732ee686eac2233de9a84a2b4
petermichon <peter.michon@proton.me>
2025-01-30 16:11:09 +0100

    Revert "Use TS version of service worker"
    This reverts commit a8070e7c502791728b33ea2f1c7b224144775639.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import main from './app/main.ts'
 if ('serviceWorker' in navigator) {
   globalThis.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/service-worker.ts')
+      .register('/service-worker.js')
       .then((registration) => {
         console.log('scope:', registration.scope) // DEBUG
       })
