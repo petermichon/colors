07718149e4e50b2573b32ce9ec41f89908170428
petermichon <peter.michon@proton.me>
2025-01-30 21:05:03 +0100

    Update main.ts

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,6 +3,21 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/Addons.js'
 
 export default function main() {
+  // ---
+
+  const keys: Record<string, boolean> = {}
+
+  // Listen for keydown and keyup events
+  globalThis.addEventListener('keydown', (event) => {
+    keys[event.code] = true
+  })
+
+  globalThis.addEventListener('keyup', (event) => {
+    keys[event.code] = false
+  })
+
+  // ---
+
   const width = globalThis.innerWidth
   const height = globalThis.innerHeight
 
@@ -26,21 +41,33 @@ export default function main() {
   const geometry = new THREE.BoxGeometry(1, 1, 1)
   const material = new THREE.MeshStandardMaterial()
 
-  {
-    const mesh = new THREE.Mesh(geometry, material)
-    mesh.position.set(-1.5, 0.5, -1.5)
-    scene.add(mesh)
-  }
-  {
-    const mesh = new THREE.Mesh(geometry, material)
-    mesh.position.set(1.5, 0.5, 1.5)
-    scene.add(mesh)
-  }
+  // function setblock(x: number, y: number, z: number) {
+  //   x += 0.5
+  //   y += 0.5
+  //   z += 0.5
+  //   const mesh = new THREE.Mesh(geometry, material)
+  //   mesh.position.set(x, y, z)
+  //   scene.add(mesh)
+  // }
+
+  // setblock(1, 0, 0)
+  // setblock(2, 0, 0)
+  // setblock(3, 0, 0)
+  // setblock(4, 0, 0)
+  // setblock(5, 0, 0)
+  // setblock(6, 0, 0)
+  // setblock(7, 0, 0)
+  // setblock(8, 0, 0)
+  // setblock(9, 0, 0)
 
   // Lights
   const light = new THREE.PointLight(0xffffff, 1)
-  light.position.set(0, 2, 0)
+  light.position.set(4.5, 3.5, 1.5)
   scene.add(light)
+
+  const player = new THREE.Mesh(geometry, material)
+  player.position.set(5.5, 0.5, 0.5)
+  scene.add(player)
 
   // Helpers
   const gridSize = 50
@@ -52,18 +79,18 @@ export default function main() {
 
   controls.update()
 
-  function animate(time: number) {
-    {
-      // Auto resize canvas
-      // const width = window.innerWidth;
-      // const height = window.innerHeight;
-      // renderer.setSize(width, height);
-    }
-
+  function animate() {
     // mesh.rotation.x = time / 2000;
     // mesh.rotation.y = time / 1000;
     // light.position.x += Math.cos(time / 1000) * 0.1;
-    light.position.z += Math.sin(time / 1000) * 0.05
+    // light.position.z += Math.sin(time / 1000) * 0.05
+
+    // Move the cube based on keyboard input
+    if (keys['KeyA']) player.position.x -= 0.1
+    if (keys['KeyD']) player.position.x += 0.1
+
+    if (keys['KeyW']) player.position.z -= 0.1
+    if (keys['KeyS']) player.position.z += 0.1
 
     renderer.render(scene, camera)
   }
